009a69d37030bc12f21731997b7336eeb2dceafd
Vivek Anand <vivekanand1101@gmail.com>
2015-06-19 02:02:44 +0530

    Implemented Flask server

--- driver.py
+++ driver.py
@@ -1,67 +1,56 @@
-from sys import path
+import sys
 from subprocess import Popen
-#from cgi import FieldStorage
+from flask import Flask, request
 
 sys.path[:0] = ['/var/www/html/opinator/database', '/var/www/html/opinator/mindwrap']
-
-from dbdriver import Driver
+from driver import Driver
 from sentiment import sentiment_calculator
 
-def drive():
 
-    """
-    data = FieldStorage()
-    product_id = str(data.getvalue('product_code'))
-    website_name = str(data.getvalue('website'))
-    url = str(data.getvalue('url'))
-    """
-    
-    # The below variables are for testing purpose, the actual data has to come
-    # from plugin
-    product_id = 'B00VEB055E'
-    website_name = 'AmazonIN'
-    url = 'http://www.amazon.in/gp/product/B00VEB055E/ref=s9_ri_gw_g23_i2?pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=center-3&pf_rd_r=0X4SSMGC8SMPYGX1QV2P&pf_rd_t=101&pf_rd_p=609217867&pf_rd_i=1320006031'
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def drive():
+    product_id = request.json['product_id']
+    url = request.json['url']
+    website_name = request.json['website_name']
+
     query_obj = Driver(website_name=website_name, product_id=product_id, action='query')
     status, sentiment = query_obj.drive()
 
     update_flag = 0
     if status == 'outdated':
-        print 'outdated'
+        # The product has been searched before but is outdated
         update_flag = 1
     elif status == 'present':
-        print 'already present in db'
-        print 'sentiment:', sentiment
-        exit()
-        # return 'sentiment' to plugin code
+        # The product is present in the database and there is no need of
+        # further actions
+        return sentiment
     elif status == 'absent':
-        print 'new product'
         # this implies the product is new
 
-    """
+    # Run the scraper
     p = Popen(['scrapy', 'crawl', '-a', 'product_id=%s' % product_id , 'revscraper'], cwd='/var/www/html/opinator/scraper')
     p.wait()
-    print 'Scraper working'
-    """
+
+    # sentiment_calculator() is function which uses coreNLP to give back
+    # sentiment_score and sentiment
     sentiment_score, sentiment = sentiment_calculator()
-    print 'sentiment_score:', sentiment_score
-    print 'sentiment:', sentiment
 
-    #################
-    # code to return sentiment to the plugin goes here
-    ################
-
-    # update the database
+    #update the database
     if update_flag == 1:
+        # Update the database with the new sentiment scores and sentimet of the
+        # earlier existing product
         update_obj = Driver(website_name=website_name, product_id=product_id, action='update', url=url,
                                 sentiment_score=sentiment_score, sentiment=sentiment)
         update_obj.drive()
-        print 'updated'
-        return
     else:
+        # Update the database with a new product
         insert_obj = Driver(website_name=website_name, product_id=product_id, action='insert', url=url,
                                 sentiment_score=sentiment_score, sentiment=sentiment)
         insert_obj.drive()
-        print 'inserted'
-        return
 
-drive()
+    return sentiment
+
+# Run the flask server
+app.run(debug=True)
